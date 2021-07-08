791316bee8f8b481590d7ab1ccb680edf172dbe6
Juan José Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-07-08 07:19:53 -0500

    final 4 class

---
+++ class-4/hello-world/src/components/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  const name = "Juan Jaramillo"
+  return(
+    <>
+      <h1>Hello World</h1>
+      <h2>{
+      }</h2>
+    </>
+  );
+}
+
+export default HelloWorld;
---
+++ class-4/hello-world/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+
+const element = document.getElementById("root");
+
+// ReactDOM.render(_que_mostrar, donde_mostrar);
+ReactDOM.render(<HelloWorld />,element);
